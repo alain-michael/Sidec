550d1b18c33b83f1a26a04486e61e55bb3e23495
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2023-12-05 12:44:01 +0100

    new

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 /* Toggle menu */
 
+let arrow = document.querySelectorAll(".arrow");
+
+for (var i = 0; i < arrow.length; i++) {
+    arrow[i].addEventListener("click", (e) => {
+        let arrowParent = e.target.parentElement.parentElement;
+        arrowParent.classList.toggle("ShowMenu");
+    });
+}
+    let sidebar = document.querySelector(".sidebar");
+    let sidebarbtn = document.querySelector(".ham_menu");
+    sidebarbtn.addEventListener("click", () => {
+        sidebar.classList.toggle("close")
+    })
+
+    /* script for profile */
+    let profile = document.querySelector('.home-section .home-content .flex .profile-2');
+
+    document.querySelector('#user-btn').onclick = () =>{
+        profile.classList.toggle('active');
+        SearchForm.classList.remove('active');
+    }
+
+    let SearchForm = document.querySelector('.home-section .home-content .flex .search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    SearchForm.classList.toggle('active');
+}
+
+    window.onscroll = () => {
+        profile.classList.remove('active');
+        SearchForm.classList.remove('active');
+    }
