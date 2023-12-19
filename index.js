52f4d001e18a8374d80cd4e424538a1afb452f10
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2023-12-19 14:06:42 +0100

    updating some contents

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
   
 
 // --------------------for responsive side bar
-function myFunction(x) {
+function myFunction1(x) {
     if (x.matches) { // If media query matches
       document.querySelector(".ham_menu").click()
     } 
@@ -58,13 +58,13 @@ function myFunction(x) {
   
   // Call listener function at run time
   setTimeout(()=>{
-    myFunction(x);
+    myFunction1(x);
   },10)
 
   
   // Attach listener function on state changes
   x.addEventListener("change", function() {
-    myFunction(x);
+    myFunction1(x);
   });
          
 /* Toggle menu */
@@ -231,4 +231,43 @@ function addTask() {
     }
 }
 
+//function for search in questions tab
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction3() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
   
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
