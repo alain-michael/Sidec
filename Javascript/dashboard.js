7f5e092cc043a02850e0ebd86831579f38453fde
Simeon-Azeh <144020513+Simeon-Azeh@users.noreply.github.com>
2024-01-26 06:15:48 +0100

    Profile card completed

--- Javascript/dashboard.js
+++ Javascript/dashboard.js
@@ -247,22 +247,7 @@ prevNextIcon.forEach(icon => { // getting prev and next icons
   });
   
 
-  //script for acordion 
 
-  var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}
 //script 
 // JavaScript code for course tabs
 function openTab(evt, tabName) {
